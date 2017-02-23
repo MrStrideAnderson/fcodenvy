11bc26b69cf3f4b3d628464ac72b66ec62713a76
MrStrideAnderson <stride_anderson@qq.com>
2017-02-23 17:00:33 +0000

    1303undone

--- Hello.c
+++ Hello.c
@@ -9,23 +9,49 @@
 #define sl long
 #define sd double
 
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+
+#define ui unsigned int
+#define si int
+#define ul unsigned long
+#define sl long
+#define sd double
 
 
+char days[200001][2];
+long n;
+int partition(char arr[][2], long low, long high){
+   char key;
+    key = arr[low][0];
+    while(low<high){
+        while(low <high && arr[high][0]>= key )
+            high--;
+        if(low<high)
+            arr[low++][0] = arr[high][0];
+        while( low<high && arr[low][0]<=key )
+            low++;
+        if(low<high)
+            arr[high--][0] = arr[low][0];
+    }
+    arr[low][2] = key;
+    return low;
+}
+void quick_sort(char arr[][2], long start, long end){
+   long pos;
+    if (start<end){
+        pos = partition(arr, start, end);
+        quick_sort(arr,start,pos-1);
+        quick_sort(arr,pos+1,end);
+    }
+    return;
+}
+
+   
 int main(int num ,char *arg[])
 {
-   int a, b, c, d, e;
-    int x, y, z;
-    scanf("%d%d%d%d%d",&a, &b, &c, &d, &e);
-    x = a-2*e;
-    y = b-2*e;
-    z = c-2*e;
-
-
-
-
-    printf("%d ",(2 * (a*b + b*c + c*a)) + 8 * x* d + 8 * y* d + 8 * z* d);
-
-    printf("%d\n", a * b * c - 2 * x * y * d - 2 * x * z * d - 2 * y * z * d );
-
+ 
+  //quick_sort(arr,0,N-1);
     return 0;
 }
